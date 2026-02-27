a09f48e337a88f532328b3d71b6b58af7e3d2d0c
ihor <Ihor.Halaniuk@gmail.com>
2026-02-28 01:28:23 +0200

    0. Add Black-Scholes-Merton model for eu options.

--- tests/bsm_test.py
+++ tests/bsm_test.py
@@ -1,4 +1,4 @@
-from math import isclose
+from math import exp, isclose
 
 from jj_qfin_math.bsm import bsm_option_eu_prices
 
@@ -16,3 +16,21 @@ def test_bsm_option_eu_prices_prob_14_4():
 
     assert isclose(c, 3.61044, rel_tol=0, abs_tol=1e-4)
     assert isclose(p, 2.37594, rel_tol=0, abs_tol=1e-4)
+
+
+def test_bsm_option_eu_prices_prob_14_5():
+    d = 1.50
+    dt = 2 / 12
+    r = 10 / 100
+    c, p = bsm_option_eu_prices(
+        s0=50 - d * exp(-r * dt),
+        k=50,
+        t=3 / 12,
+        r=r,
+        sigma=30 / 100,
+    )
+
+    print(c, p)
+
+    assert isclose(c, 2.7895, rel_tol=0, abs_tol=1e-4)
+    assert isclose(p, 3.0302, rel_tol=0, abs_tol=1e-4)
